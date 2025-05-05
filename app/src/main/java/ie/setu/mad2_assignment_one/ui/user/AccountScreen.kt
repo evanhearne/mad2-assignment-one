e47cfee66da0b950dc5fba66f7b6f112e4b4c7f9
Evan Hearne <evanhearne02@gmail.com>
2025-05-05 15:48:57 +0100

    add upload + display user photo

--- app/src/main/java/ie/setu/mad2_assignment_one/ui/user/AccountScreen.kt
+++ app/src/main/java/ie/setu/mad2_assignment_one/ui/user/AccountScreen.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
@@ -17,11 +19,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import coil3.compose.AsyncImage
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
 import ie.setu.mad2_assignment_one.ui.BottomNavigationBar
@@ -44,10 +48,18 @@ fun AccountScreen(
             var user by remember { mutableStateOf(Firebase.auth.currentUser) }
             if (user != null) {
                 // check if user logged in
+                // User Photo
+                Row(modifier = Modifier
+                    .align(Alignment.CenterHorizontally)) {
+                    AsyncImage(user!!.photoUrl, contentDescription = "${user!!.displayName} profile picture ", modifier = Modifier.size(250.dp).clip(
+                        CircleShape
+                    ))
+                }
+                // Display name
                 Row(modifier = Modifier
                     .align(Alignment.CenterHorizontally)
                     .padding(bottom = 20.dp)) {
-                    Text(text ="Hello ${user!!.email} ! ", fontSize = 25.sp) // TODO change this to user.displayName after auth rework.
+                    Text(text ="Hello ${user!!.displayName} ! ", fontSize = 25.sp)
                 }
                 Row(modifier = Modifier.align(Alignment.CenterHorizontally)) {
                     Button(onClick = {
